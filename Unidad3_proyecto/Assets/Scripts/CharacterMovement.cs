facbade42e274947decd28ca26becedde5b2277c
Esmeralda del Socorro Orozco Diaz <esmeralda.orozco24a@udgvirtual.udg.mx>
2026-02-27 14:27:26 -0600

    Actividad 3.1

--- Unidad3_proyecto/Assets/Scripts/CharacterMovement.cs
+++ Unidad3_proyecto/Assets/Scripts/CharacterMovement.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class CharacterMovement : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float Horizontal;
     [SerializeField] private float velocidad = 5f;
     public float JumpForce;
+    private bool Grounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();//Nos permite acceder a Rigidbody2D
+        Animator = GetComponent<Animator>();//Nos permite acceder a Animator
     }
 
     // Update is called once per frame
@@ -21,7 +25,19 @@ public class CharacterMovement : MonoBehaviour
             Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed ? -1 :
             Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed ? 1 : 0;
 
-        if (Keyboard.current.wKey.wasPressedThisFrame || Keyboard.current.upArrowKey.wasPressedThisFrame)
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        Animator.SetBool("running", Horizontal != 0.0f);
+
+        Debug.DrawRay(transform.position, Vector3.down * 1.2f, Color.blue);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 1.2f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        if ((Keyboard.current.wKey.wasPressedThisFrame || Keyboard.current.upArrowKey.wasPressedThisFrame) && Grounded)
         {
             Jump();
         }
